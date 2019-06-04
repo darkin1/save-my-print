0832032fa703218be131467514b98750e1c6f06d
Dariusz Ciesielski <dciesielski87@gmail.com>
2019-06-04 09:47:01 +0200

    Predict model endpoint and service

--- app/api/landing/views.py
+++ app/api/landing/views.py
@@ -2,17 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+
 def show(request):
-    return render(request, 'landing/show.html', context = {})
+    return render(request, 'landing/show.html', context={})
+
 
 def privacy(request):
-    return render(request, 'landing/privacy.html', context = {})
+    return render(request, 'landing/privacy.html', context={})
+
 
 def terms(request):
-    return render(request, 'landing/terms.html', context = {})
+    return render(request, 'landing/terms.html', context={})
+
 
 def assets(request):
-    return render(request, 'landing/assets.html', context = {})
+    return render(request, 'landing/assets.html', context={})
+
 
 def form(request):
     return HttpResponse("This is form")
---
+++ app/api/predict.py
@@ -0,0 +1,54 @@
+import skimage
+from skimage.transform import resize
+import os
+import numpy as np
+import json
+import requests
+from matplotlib import pyplot as plt
+
+def show_image(X):
+    plt.imshow(np.squeeze(X), cmap="gray")
+    plt.show()
+
+IMG_SIZE = 160
+path = './../static/dataset_test'
+## FAILED
+# img = '0004.jpg'
+# img = 'B0tLsXHCYAAAeq-.jpg'
+# img = 'DwX7Vz9uVgdx_HqV0-gefJ4FI8btsuB3BmmtrgCIvi4.jpg'
+# img = 'fsc.jpg'
+
+## GOOD
+path = './../static/dataset_test/good'
+# img = '5e04faf6b1ebee0735ffb82771ca9051_preview_featured.jpg'
+img = 'BV-Acharya-9.jpg'
+# img = 'fused-filament-fabrication-fff-thumb-600x300.jpg'
+# img = 'index.jpg'
+
+
+
+
+image = skimage.io.imread(os.path.join(path, img))
+image = resize(image, (IMG_SIZE, IMG_SIZE))
+
+# If grayscale. Convert to RGB for consistency.
+print(image.ndim)
+# if image.ndim != 3:
+#     image = skimage.color.gray2rgb(image)
+
+# image = 1 - np.array(image).astype('float32') / 255.
+# image = image/255
+image = 1 - np.array(image).astype('float32')
+show_image(image)
+
+
+# data = json.dumps({"signature_name": "serving_default", "instances": image[0:3].tolist()})
+data = json.dumps({"signature_name": "serving_default", "instances": [image.tolist()]})
+
+
+headers = {"content-type": "application/json"}
+json_response = requests.post('http://localhost:8501/v1/models/half_plus_two:predict', data=data, headers=headers)
+# predictions = json.loads(json_response.text)['predictions']
+predictions = json.loads(json_response.text)
+
+print(predictions)
